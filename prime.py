9b1d3e83647c0110ab8f25510a6d48f0feab0b04
Hema113 <gowthamh113@gmail.com>
2019-06-12 13:12:24 +0530

    shows 2 is not prime and 9 is prime

--- prime.py
+++ prime.py
@@ -5,7 +5,7 @@ def prime_no(num):
     #given number is greater than 1
 
     if num > 1:
-        for i in range(2, num//2):
+        for i in range(2, num):
             if (num % i) == 0:
                 return False
                 break
@@ -17,6 +17,6 @@ if __name__ == "__main__":
 
     num=int(input("To check given its prime or not:"))
     if prime_no(num):
-        print("Given number is not prime number", num)
+        print("Given number is  prime number", num)
     else:
-        print("Given Number is prime number", num)
+        print("Given Number is not prime number", num)
