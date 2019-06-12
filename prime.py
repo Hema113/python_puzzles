e42599f22051d965f5d5643b0009f133ea2b1467
Hema113 <gowthamh113@gmail.com>
2019-06-12 10:56:18 +0530

    Modifed pgm

--- prime.py
+++ prime.py
@@ -1,8 +1,22 @@
+#check wheather given num is prime or not
+
 def prime_no(num):
-      if (num%2)==0:
-          print(num,"is a not prime number")
-      else:
-           print(num," is a prime number")
+
+    #given number is greater than 1
+
+    if num > 1:
+        for i in range(2, num//2):
+            if (num % i) == 0:
+                return False
+                break
+            else:
+                return True
+
 if __name__ == "__main__":
+    #Getting input from user
+
     num=int(input("To check given its prime or not:"))
-    prime_no(num)
+    if prime_no(num):
+        print("Given number is not prime number", num)
+    else:
+        print("Given Number is prime number", num)
