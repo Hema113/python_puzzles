b4b703d33051908209976c490592c940669033fe
Hema113 <gowthamh113@gmail.com>
2019-06-17 11:31:32 +0530

    Final program

--- gcd.py
+++ gcd.py
@@ -1,10 +1,13 @@
-def gcd(a, b):
-    while(a != 0):
-        temp = b
-        a % b == 0
-        temp = a
-    return a
+# Find GCD of two numbwrs
+def gcd(n1, n2):
+
+    #usewhile loop find gcd
+    while(n2!=0):
+        temp = n2
+        n2 = n1%n2
+        n1 = temp
+    return n1
 if __name__ == "__main__":
-    a = int(input("Enter the values of num A>>>"))
-    b = int(input("Enter the value of Num B>>>"))
-    print(gcd(a, b))
+    n1 = int(input("Enter the values of num A>>>"))
+    n2 = int(input("Enter the value of Num B>>>"))
+    print(gcd(n1, n2))
