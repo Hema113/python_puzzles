ec6847544c46de3c87c27a65ac99b9d0340ec9cc
Hemachandran N <gowthamh113@gmail.com>
2019-06-18 17:20:52 +0530

    Delete primeBackwardprint.py

--- primeBackwardprint.py
+++
@@ -1,8 +0,0 @@
-def prime_no(num):
-      if (num%2)==0:
-          print(num-3,"is a not prime number")
-      else:
-          return print(num-3," is a prime number")
-if __name__ == "__main__":
-    num=int(input("Enter the number check its prime or not:"))
-    prime_no(num)
