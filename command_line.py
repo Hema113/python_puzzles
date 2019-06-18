46b367b3c0e4537ed8aecc301b4b2b1ceecb6dd7
Hema113 <gowthamh113@gmail.com>
2019-06-18 17:38:25 +0530

    comment line argument print

--- command_line.py
+++ command_line.py
@@ -1,6 +1,16 @@
+# This program returns commaline arugument
 import sys
 
+# This function prints user input at command line
+def command(arg, arg1, arg2):
+    print(arg)
+    print(arg1)
+    print(arg2)
+    
 
-#if __name__ == "__main__":
-arg =sys.argv[0]
-arg1=sys.argv[1]
+# Main
+if __name__ == "__main__":
+    arg = sys.argv[0]
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    print(command(arg, arg1, arg2))
