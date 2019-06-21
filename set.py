37eb814ba1db7892a0724a7ae33f6d03aced1a52
Hema113 <gowthamh113@gmail.com>
2019-06-21 15:37:49 +0530

    some canges done

--- set.py
+++ set.py
@@ -1,11 +1,13 @@
 # Prgram for Eliminate duplicate elements in list usein set
 
 def eliminate(data):
+    result1 = []
     result =set(data) #---> Convert list to set
-    return result
+    result1 =list(result)
+    return result1
 
 # Main
 if __name__ == "__main__":
     data = [1, 2, 2, 3, 4, 5, 5 ,6 ,7 ,8 ,9 ,9,10]
     print("Before",data)
-    print("After useing set",eliminate(data))
+    print("After using set",eliminate(data))
