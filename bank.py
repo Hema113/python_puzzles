7781cb3a323be1f459309a9203b4a5756de70e62
Hema113 <gowthamh113@gmail.com>
2019-06-25 18:08:51 +0530

    some changes done

--- bank.py
+++ bank.py
@@ -1,15 +1,17 @@
+# program for caiculateing bank balance
+
 def deposit():
     current_bal = 0
     while True:
         in_put = input("Enter the amount>>>").split()
-        if not in_put:
+        if not in_put: #----> if input is empty then exit from the loop-
             break;
 
-        amount = int(in_put[1])
+        amount = int(in_put[1]) # Convert user input string to int it takes the list[1] value it means 100
         if in_put[0] == "d" or in_put[0] == "D":
-            current_bal += amount
+            current_bal += amount #---->Deposit amt calcultion
         elif in_put[0] == "w" or in_put[0] == "W":
-            current_bal -= amount
+            current_bal -= amount #------> Withdraw amt calculation
         #elif in_put == 'exit':
         #    break
 
