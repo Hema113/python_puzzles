4ee60e21c64042ad04162cd1df3a551e76cd72e1
Hema113 <gowthamh113@gmail.com>
2019-06-26 18:44:53 +0530

    some fuction will be add

--- calculator.py
+++ calculator.py
@@ -10,16 +10,23 @@ def mul(num1, num2):
 def div(num1, num2):
     return num1 / num2
 
+def odd_even(num1):
+    if num % 2 == 0:
+        return ("Even>>>",num)
+    else:
+        return ("odd>>>", num)
+
 print("Select your choice")
 print("1.add")
 print("2.sub")
 print("3.mul")
 print("4.div")
+print("5.odd_even")
 
-choice = input("Enter your choice(1/2/3/4):")
-
-num1 = int(input("Enter num1>>"))
-num2 = int(input("Enter num2>>"))
+choice = input("Enter your choice(1/2/3/4/5):")
+#num = int(input("Enter number>>"))
+num1 = int(input("Enter num>>"))
+num2 = int(input("Enter num>>"))
 
 if choice == '1':
     print(add(num1, num2))
@@ -29,5 +36,7 @@ elif choice == '3':
     print(mul(num1, num2))
 elif choice == '4':
     print(div(num1, num2))
+elif choice == '5':
+    print(odd_even(num))
 else:
     print("invaild input")
