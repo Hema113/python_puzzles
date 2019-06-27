5095189ff609f7622972a3c8460717677c998984
Hema113 <gowthamh113@gmail.com>
2019-06-27 12:56:49 +0530

    some changes done and bug will have

--- dictionaries.py
+++ dictionaries.py
@@ -1,16 +1,45 @@
 # Program return date of birth based on name useing dictionaries
 
 # Function for dictionaries
-def dictionary(name):
-    datas = {
-            "Hema"    : "30-04-1995",
-            "Balaji"  : "24-11-1964",
-            "Aswini"  : "29-09-1996",
-            "Krithik" : "25-08-2002"
-            }
-    return datas[name]
+def dictionary(birthday_data,name):
+    temp = []
+    for data in birthday_data["birthday"]:
+        if ["Name"] == name:
+            temp.append((data["DOB"], data["ID"])
+        else:
+            break
+    return temp
+
 
 # Main
 if __name__ == "__main__":
+    birthday_data = {
+            "birthday":
+            [
+            {
+            "ID": 1,
+            "Name": "Hemachandran",
+            "DOB": "30-06-1995"
+            },
+            {
+            "ID": 2,
+            "Name": "Hemachandran",
+            "DOB": "30-04-1996"
+            }
+            {
+            "ID": 30,
+            "Name": "Balaji",
+            "DOB": "24-011-1964"
+            }
+            ]
+            }
+
     name = input("Enter the name>>>")
-    print(dictionary(name))
+    result = dictionary(birthday_data,name)
+
+    if result == []:
+        print("Data not found")
+
+     else:
+         for i in result:
+             print(name,"DOB>>>",i[0],"ID>>>",i[1])
